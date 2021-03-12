db96175096bad98155fe6a7b1550e7f681dbbf7d
hadtrindade <haddleytrindade@gmail.com>
2021-03-12 18:40:24 -0300

    criação do app core e do model de usuário.
    close #7

--- calling_plan/urls.py
+++ calling_plan/urls.py
@@ -13,11 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
-import debug_toolbar
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('__debug__/', include(debug_toolbar.urls))
+    path("", include('calling_plan.pages.urls')),
+    path("", include('calling_plan.core.urls'))
 ]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns.append(
+        path('__debug__/', include(debug_toolbar.urls))
+        )
